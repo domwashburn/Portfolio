823715deb781f304074ea2c652a5f58e11973acb
Dominick Washburn <domwashburn@us.ibm.com>
2017-04-15 20:20:02 -0500

    feat(style): heading styles
    beautiful typographic styling for the welcome banner with a fallback for browsers that don't support
    background-clip-text

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="hue-rotate">
-        <h1 className="title--lg">Hey there! <span class="text--bold">I'm Dominick</span></h1>
-        <h2 className="title--md">I’m a <span className="text--bold">UI Architect</span> and <span className="text--bold">Front End Developer.</span></h2>
-        <h2 className="title--md">I’m currently working for <span className="text--bold">IBM</span> as a <span className="text--bold">Front End Development Lead</span> on our <span className="text--bold">Private Cloud Analytics design team.</span></h2>
-        <p className="copy">I believe that beautiful web design is more than pixel deep, so I craft code and
-  architecture as polished as the interfaces I’m creating.</p>
-        <div className="background"></div>
-        <Typekit kitId="fjf7erl" />
-      </div>
+      <section>
+        <div className="welcome hue-rotate hue-text">
+          <div className="welcome__copy">
+            <h1 className="title">Hey there, <span className="text--bold">I'm Dominick!</span></h1>
+            <h2 className="sub-title">I’m a <span className="text--bold">UI Architect</span> and <span className="text--bold">Front End Developer.</span></h2>
+            <h2 className="sub-title">I’m currently working for <span className="text--bold">IBM</span> as a <span className="text--bold">Front End Development Lead</span> on our <span className="text--bold">Private Cloud Analytics design team.</span></h2>
+            <p className="copy">I believe that beautiful web design is more than pixel deep, so I craft code and
+      architecture as polished as the interfaces I’m creating.</p>
+          </div>
+          <div className="welcome__background"></div>
+        </div>
+          <Typekit kitId="fjf7erl" />
+      </section>
     );
   }
 }
